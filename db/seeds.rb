468786dfca0465c5e9aa60a756363a6c877c6dca
Andrew Wever <andrew.wever@gmail.com>
2022-11-09 16:52:43 -0500

    basic site working, but no filters

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+27.times do
+  flat = Flat.create!(
+    name: "#{Faker::Adjective.negative.capitalize} #{Faker::Adjective.negative} #{Faker::Marketing.buzzwords}",
+    address: Faker::Address.street_address,
+    description: "#{Faker::Quote.famous_last_words}\n\n#{Faker::Quotes::Shakespeare.hamlet_quote}",
+    price_per_night: rand(15..999),
+    number_of_guests: rand(1..12),
+    image_url: "https://source.unsplash.com/random/?interior"
+  )
+  puts "Flat #{flat.name} created"
+end
+
+puts "Finished!"
